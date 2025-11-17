5809e555e3b82fe44217084fcd7e3f5c611e13fc
CristianTambaco <1234cristiant@gmail.com>
2025-11-16 20:30:04 -0500

    modified:   frontend/src/components/create/Form.jsx

--- frontend/src/components/create/Form.jsx
+++ frontend/src/components/create/Form.jsx
@@ -1,13 +1,13 @@
+// frontend/src/components/create/Form.jsx
 import { useState, useEffect } from "react";
 import useFetch from "../../hooks/useFetch"
 import { useNavigate } from "react-router"
 import { useForm } from "react-hook-form"
 import {generateAvatar,convertBlobToBase64} from "../../helpers/consultarIA"
 import { toast, ToastContainer } from "react-toastify"
-
+import storeAuth from "../../context/storeAuth"; // <-- Importar storeAuth
 
 export const Form = ({patient}) => {
-
 
     const [avatar, setAvatar] = useState({
         image: "https://cdn.pixabay.com/photo/2017/01/25/17/35/picture-2008484_1280.png",
@@ -18,10 +18,9 @@ export const Form = ({patient}) => {
     const navigate = useNavigate()
     const { register, handleSubmit, formState: { errors }, setValue, watch, reset } = useForm()
     const { fetchDataBackend } = useFetch()
-
+    const { rol } = storeAuth(); // <-- Obtener el rol del usuario autenticado
 
     const selectedOption = watch("imageOption")
-
 
     const handleGenerateImage = async () => {
         setAvatar(prev => ({ ...prev, loading: true }))
@@ -30,7 +29,7 @@ export const Form = ({patient}) => {
             // blob:http://localhost/ea27cc7d-
             const imageUrl = URL.createObjectURL(blob)
             // data:image/png;base64,iVBORw0KGg
-            const base64Image = await convertBlobToBase64(blob)           
+            const base64Image = await convertBlobToBase64(blob)
             setAvatar(prev => ({ ...prev, image: imageUrl, loading: false }))
             setValue("avatarMascotaIA", base64Image)
         }
@@ -41,34 +40,23 @@ export const Form = ({patient}) => {
         }
     }
 
-
-
     const registerPatient = async (data) => {
-        /*
-        const data = {
-            nombre: "Firulais",
-            edad: "2",
-            imagen: [File]  // un array con 1 imagen cargada por el usuario
-        }
-        */
-        // clase de JavaScript ideal para enviar datos como texto + imágenes al servidor
         const formData = new FormData()
-        // Recorre todos los elementos del formulario
         Object.keys(data).forEach((key) => {
             if (key === "imagen") {
-                formData.append("imagen", data.imagen[0]) // se guarda el archivo real
+                formData.append("imagen", data.imagen[0])
             } else {
-                formData.append(key, data[key]) // se guardan nombre y edad
+                formData.append(key, data[key])
             }
         })
-        
+
         let url = `${import.meta.env.VITE_BACKEND_URL}/cliente/registro`
         const storedUser = JSON.parse(localStorage.getItem("auth-token"))
         const headers= {
                 "Content-Type": "multipart/form-data",
                 Authorization: `Bearer ${storedUser.state.token}`
             }
-        
+
         let response
         if (patient?._id) {
             url = `${import.meta.env.VITE_BACKEND_URL}/cliente/actualizar/${patient._id}`
@@ -93,16 +81,13 @@ export const Form = ({patient}) => {
                 celularPropietario: patient?.celularPropietario,
                 nombreMascota: patient?.nombreMascota,
                 tipoPelajeMascota: patient?.tipoPelajeMascota,
-                // fechaNacimientoMascota: new Date(patient?.fechaNacimientoMascota).toLocaleDateString('en-CA', {timeZone: 'UTC'}),
                 caracteristicasMascota: patient?.caracteristicasMascota,
-                estadoMascota: patient.estadoMascota.toString(), // Convertir booleano a string para el select
+                estadoMascota: patient.estadoMascota?.toString(), // Convertir booleano a string para el select
             })
         }
-    }, [patient, reset]) // Asegurar que se ejecute cuando 'patient' cambie
+    }, [patient, reset])
 
-
-    
-        return (
+    return (
         <form onSubmit={handleSubmit(registerPatient)}>
             <ToastContainer />
 
@@ -111,22 +96,17 @@ export const Form = ({patient}) => {
                 <legend className="text-xl font-bold text-gray-700 bg-gray-200 px-4 py-1 rounded-md">
                     Información del propietario
                 </legend>
-
                 {/* Cédula */}
                 <div>
-
                     <label className="mb-2 block text-sm font-semibold">
                         Cédula <span className="text-red-600">*</span>
                     </label>
-
-
                     <div className="flex items-center gap-10 mb-5">
                         <input
                             type="number"
                             placeholder="Ingresa la cédula"
                             className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500"
                             {...register("cedulaPropietario", { required: "La cédula es obligatoria",
-
                                 minLength: {
                                     value: 10,
                                     message: "La cédula debe tener al menos 10 dígitos",
@@ -141,33 +121,22 @@ export const Form = ({patient}) => {
                                     noCeroInicial: (value) =>
                                         !/^0/.test(value) || "La cédula no puede comenzar con 0",
                                 },
-                                                                           
-
                             })}
                         />
-                        {/* <button className="py-1 px-8 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white sm:w-80"
-                        disabled={patient}
-                        >
-                            Consultar
-                        </button> */}
                     </div>
                     {errors.cedulaPropietario && <p className="text-red-800">{errors.cedulaPropietario.message}</p>}
                 </div>
 
                 {/* Nombre completo */}
                 <div>
-
                     <label className="mb-2 block text-sm font-semibold">
                         Nombres completos <span className="text-red-600">*</span>
                     </label>
-
-
                     <input
                         type="text"
                         placeholder="Ingresa nombre y apellido"
                         className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
                         {...register("nombrePropietario", { required: "El campo es obligatorio",
-
                             minLength: {
                                 value: 5,
                                 message: "Debe tener al menos 5 caracteres",
@@ -186,51 +155,40 @@ export const Form = ({patient}) => {
                                 sinEspaciosExtremos: (value) =>
                                     value.trim() === value || "No debe iniciar o terminar con espacios",
                             },
-                         
-
-                         })}
+                        })}
                     />
                     {errors.nombrePropietario && <p className="text-red-800">{errors.nombrePropietario.message}</p>}
                 </div>
 
                 {/* Correo electrónico */}
                 <div>
-
-
                     <label className="mb-2 block text-sm font-semibold">
                         Correo electrónico <span className="text-red-600">*</span>
                     </label>
-
-
                     <input
                         type="emaile"
                         placeholder="Ingresa el correo electrónico"
                         className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
                         {...register("emailPropietario", { required: "El correo electrónico es obligatorio",
-
                             pattern: {
                                 value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                                 message: "Ingresa un correo electrónico válido"
                             }
-
-                         })}
+                        })}
                     />
                     {errors.emailPropietario && <p className="text-red-800">{errors.emailPropietario.message}</p>}
                 </div>
 
                 {/* Celular */}
                 <div>
-
                     <label className="mb-2 block text-sm font-semibold">
                         Celular <span className="text-red-600">*</span>
                     </label>
-
                     <input
                         type="number"
                         placeholder="Ingresa el celular"
                         className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
                         {...register("celularPropietario", { required: "El celular es obligatorio",
-
                             pattern: {
                                     value: /^[0-9]+$/,
                                     message: "El teléfono solo puede contener números"
@@ -243,12 +201,10 @@ export const Form = ({patient}) => {
                                     value: 10,
                                     message: "El teléfono debe tener máximo 10 dígitos"
                                 },
-
-                                validate: value => {                
-                                if (/^0+$/.test(value)) return "Teléfono inválido"; // todo ceros                
+                                validate: value => {
+                                if (/^0+$/.test(value)) return "Teléfono inválido"; // todo ceros
                                 }
-
-                         })}
+                        })}
                     />
                     {errors.celularPropietario && <p className="text-red-800">{errors.celularPropietario.message}</p>}
                 </div>
@@ -262,20 +218,14 @@ export const Form = ({patient}) => {
 
                 {/* Nombre de la mascota */}
                 <div>
-
-                    
                     <label className="mb-2 block text-sm font-semibold">
                         Nombre <span className="text-red-600">*</span>
                     </label>
-
-
-
                     <input
                         type="text"
                         placeholder="Ingresar nombre"
                         className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
                         {...register("nombreMascota", { required: "El campo es obligatorio",
-
                             minLength: {
                                 value: 5,
                                 message: "El nombre debe tener al menos 5 caracteres"
@@ -288,128 +238,16 @@ export const Form = ({patient}) => {
                                     value: 20,
                                     message: "El nombre no puede superar los 20 caracteres"
                                 },
-
-                         })}
+                        })}
                     />
                     {errors.nombreMascota && <p className="text-red-800">{errors.nombreMascota.message}</p>}
                 </div>
 
-
-                        {/* ------------ */}
-
-                {/* Imagen de la mascota*/}
-
-                {/* <label className="mb-2 block text-sm font-semibold">Imagen</label> */}
-                <div className="flex gap-4 mb-2">
-                    
-                    {/* Opción: Imagen con IA */}
-
-
-                    {/* <label className="flex items-center gap-2">
-                        <input
-                            type="radio"
-                            value="ia"
-                            {...register("imageOption")}
-
-                            // {...register("imageOption",{ required: !patient && "Seleccione una opción"})}
-                            // disabled={patient}
-                        />
-                        Sin imagen
-                    </label> */}
-
-
-
-                    {/* Opción: Subir Imagen */}
-                    {/* <label className="flex items-center gap-2">
-                        <input
-                            type="radio"
-                            value="upload"
-                            // {...register("imageOption",{ required: !patient && "Seleccione una opción"})}
-
-                            {...register("imageOption")}
-
-                            // disabled={patient}
-                        />
-                        Subir Imagen
-                    </label>
-                    {errors.imageOption && <p className="text-red-800">{errors.imageOption.message}</p>} */}
-
-                    
-                </div>
-
-                            {/* ---------------- */}
-
-
-
-                {/* Imagen con IA */}
-                {selectedOption === "ia" && (
-                    <div className="mt-5">
-
-                        {/* <label className="mb-2 block text-sm font-semibold">Imagen con IA</label> */}
-
-                        {/* <div className="flex items-center gap-10 mb-5">
-
-                            <input
-                                type="text"
-                                placeholder="Ingresa el prompt"
-                                className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500"
-                                value={avatar.prompt}
-                                onChange={(e) => setAvatar(prev => ({ ...prev, prompt: e.target.value }))}
-                            />
-
-                            <button
-                                type="button"
-                                className="py-1 px-8 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white sm:w-80"
-                                onClick={handleGenerateImage}
-                                disabled={avatar.loading}
-                            >
-                                {avatar.loading ? "Generando..." : "Generar con IA"}
-                            </button>
-
-
-                        </div> */}
-
-
-
-                        {/* {avatar.image && (
-                            <img src={avatar.image} alt="Avatar IA" width={100} height={100} />
-                        )} */}
-                        
-                    </div>
-                )}
-
-
-
-
-                    {/* ----------- */}
-
-
-                {/* Subir Imagen */}
-
-                {/* {selectedOption === "upload" && (
-                    <div className="mt-5">
-                        <label className="mb-2 block text-sm font-semibold">Subir Imagen</label>
-                        <input
-                            type="file"
-                            className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
-                            {...register("imagen")}
-                        />
-                    </div>
-                )} */}
-
-                {/* ----------- */}
-
-
-
                 {/* Tipo de mascota */}
                 <div>
-
-
                     <label className="mb-2 block text-sm font-semibold">
                         Sociable con otros animales <span className="text-red-600">*</span>
                     </label>
-
-
                     <select
                         id='prioridad'
                         className='block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5'
@@ -418,62 +256,30 @@ export const Form = ({patient}) => {
                         <option value="">--- Seleccionar ---</option>
                         <option value="si">Si</option>
                         <option value="no">No</option>
-                        {/* <option value="otro">Otro</option> */}
-                        
                     </select>
                     {errors.tipoPelajeMascota && <p className="text-red-800">{errors.tipoPelajeMascota.message}</p>}
                 </div>
 
-                {/* Fecha de nacimiento */}
-                {/* <div>
-                    <label className="mb-2 block text-sm font-semibold">Fecha de nacimiento</label>
-                    <input
-                        type="date"
-                        className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
-                        {...register("fechaNacimientoMascota", { required: "Seleccione una fecha" })}
-                    />
-                    {errors.fechaNacimientoMascota && <p className="text-red-800">{errors.fechaNacimientoMascota.message}</p>}
-                </div> */}
-
                 {/* Detalles */}
                 <div>
-
-
                     <label className="mb-2 block text-sm font-semibold">
                         Detalles adicionales <span className="text-red-600">*</span>
                     </label>
-
-
                     <textarea
                         placeholder="Detalles"
                         className="block w-full rounded-md border border-gray-300 py-1 px-2 text-gray-500 mb-5"
                         {...register("caracteristicasMascota", { required: "El campo es obligatorio",
-
                             minLength: {
                                     value: 5,
                                     message: "Debe existir al menos 5 caracteres"
                                 },
-                                
-
-
-                         })}
+                        })}
                     />
                     {errors.caracteristicasMascota && <p className="text-red-800">{errors.caracteristicasMascota.message}</p>}
                 </div>
 
-
-                
-
-                {/* <div>
-                    <p style={{ margin: 0, fontSize: '16px' }}>Correo electrónico: {patient?.emailPropietario}</p>
-                    <p style={{ margin: 0, fontSize: '16px' }}>Contraseña: {patient?.password}</p>
-                </div> */}
-
-
-
-
-                {/* Estado de la mascota */}
-                {patient && ( // Mostrar solo si es edición
+                {/* Estado de la mascota - Ocultar si el rol es cliente */}
+                {patient && rol !== 'cliente' && ( // Mostrar solo si es edición Y el rol no es cliente
                     <div>
                         <label className="mb-2 block text-sm font-semibold">
                             Estado <span className="text-red-600">*</span>
@@ -488,13 +294,8 @@ export const Form = ({patient}) => {
                         {errors.estadoMascota && <p className="text-red-800">{errors.estadoMascota.message}</p>}
                     </div>
                 )}
-
-
-                
-
-
             </fieldset>
-            
+
             {/* Botón de submit */}
             <input
                 type="submit"
@@ -502,7 +303,6 @@ export const Form = ({patient}) => {
                 hover:bg-gray-600 cursor-pointer transition-all"
                 value={patient ? "Actualizar" : "Registrar"}
             />
-        </form> 
-
+        </form>
     )
 }
