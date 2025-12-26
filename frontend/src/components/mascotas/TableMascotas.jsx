65d514405d9775b5a6a5829006a154f1aa9aeb81
CristianTambaco <1234cristiant@gmail.com>
2025-12-26 14:01:16 -0500

    modified:   frontend/src/components/mascotas/TableMascotas.jsx

--- frontend/src/components/mascotas/TableMascotas.jsx
+++ frontend/src/components/mascotas/TableMascotas.jsx
@@ -12,7 +12,7 @@ const TableMascotas = ({ mascotas, onDelete, onEdit }) => {
                 className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
                 role="alert"
             >
-                No hay mascotas registradas.
+                Registra tu mascota.
             </div>
         );
     }
