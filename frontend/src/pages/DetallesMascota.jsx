fc73401f93b7e783595c982618bb82d2d9367512
CristianTambaco <1234cristiant@gmail.com>
2025-12-27 01:02:54 -0500

    modified:   frontend/src/pages/DetallesMascota.jsx

--- frontend/src/pages/DetallesMascota.jsx
+++ frontend/src/pages/DetallesMascota.jsx
@@ -41,7 +41,9 @@ const DetallesMascota = () => {
             <hr className='my-4 border-t-2 border-gray-300' />
             <p className='mb-8'>Este módulo te permite ver los detalles de tu mascota.</p>
             <div className="bg-white p-6 rounded-lg shadow-md">
-                {mascota.avatar && (
+
+
+                {/* {mascota.avatar && (
                     <div className="mb-4 flex justify-center">
                         <img
                             src={mascota.avatar}
@@ -49,8 +51,14 @@ const DetallesMascota = () => {
                             className="w-40 h-40 object-cover rounded-full"
                         />
                     </div>
-                )}
-                <div className="space-y-4">
+                )} */}
+
+
+
+                <div className="flex gap-8">
+
+
+                <div className="flex-grow space-y-4 ml-3">
                     <div>
                         <label className="block text-sm font-semibold">Nombre</label>
                         <p className="mt-1">{mascota.nombre}</p>
@@ -98,7 +106,37 @@ const DetallesMascota = () => {
                             {mascota.estado ? "Activo" : "Inactivo"}
                         </span>
                     </div>
+
+
+
+
+                    
+
+
+
+
+
                 </div>
+
+
+
+                    {/* Imagen a la DERECHA */}
+                    {mascota.avatar && (
+                    <div className="flex-shrink-0">
+                        <img
+                        src={mascota.avatar}
+                        alt="Avatar de la mascota"
+                        className="w-90 h-90 object-cover rounded-full mr-6"
+                        />
+                    </div>
+                    )}
+
+
+                    </div>
+
+
+
+
                 <div className="mt-6 flex justify-between">
                     <Link
                         to="/dashboard/listar-mascotas"
@@ -108,7 +146,7 @@ const DetallesMascota = () => {
                     </Link>
                     <Link
                         to={`/dashboard/editar-mascota/${id}`}
-                        className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+                        className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 mr-4"
                     >
                         Editar Mascota
                     </Link>
