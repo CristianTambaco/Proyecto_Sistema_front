9154c36a056eb5b35827d4a5c324a879195f149a
CristianTambaco <1234cristiant@gmail.com>
2025-12-27 10:19:32 -0500

    modified:   frontend/src/pages/DetallesMascota.jsx

--- frontend/src/pages/DetallesMascota.jsx
+++ frontend/src/pages/DetallesMascota.jsx
@@ -144,12 +144,14 @@ const DetallesMascota = () => {
                     >
                         Volver a Mis Mascotas
                     </Link>
-                    <Link
+
+                    {/* <Link
                         to={`/dashboard/editar-mascota/${id}`}
                         className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 mr-4"
                     >
                         Editar Mascota
-                    </Link>
+                    </Link> */}
+
                 </div>
             </div>
         </div>
